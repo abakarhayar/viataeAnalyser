dc99e95b0976a93f05def72ca2c8b607283465ce
AKO_Berenger <vlunaski@gmail.com>
2024-07-31 20:11:10 +0200

    ✨(back) config database

--- backend/djangoProject/djangoProject/settings.py
+++ backend/djangoProject/djangoProject/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -79,11 +79,11 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'OPTIONS' : { "init_command": "SET foreign_key_checks = 0;" },
-        'NAME': 'profil',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',
-        'PORT': '',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST', default='127.0.0.1'),
+        'PORT': config('DB_PORT', default=''),
     }
 }
 
